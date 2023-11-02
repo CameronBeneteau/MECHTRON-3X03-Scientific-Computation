c06a6a4406fe304c14b35dae9f0faa6dca70292d
Cameron Beneteau <cameron.beneteau@caseware.com>
2023-11-02 17:03:33 -0400

    Assignment 2 updates

--- assignments/assignment-2/assignment2_handout.jl
+++ assignments/assignment-2/assignment2_handout.jl
@@ -99,54 +99,18 @@ Output
     n: number of equally spaced points to use 	 
 """
 function subdivide(a, b, ω, tol)
+    for n in 2:1000
+        h = (b - a) / n
 
-    # Analytical soltuion
-    # n = 1
+        curr = ω^(n + 1) * h^(n + 1) / (4 * (n + 1))
 
-    # while n < 1000000
-
-    #     h = (b - a) / n
-
-    #     curr = ω^(n + 1) * h^(n + 1) / (4 * (n + 1))
-
-    #     if curr < tol
-    #         println("Subdivide Nodes: ", n)
-    #         n = 10
-    #         return n
-    #     end
-    #     n += 1
-    # end
-
-    # return 0
-
-
-    # Trial and srror solution
-    n = 2
-
-    while true
-
-        x_vals = range(a, stop=b, length=n)
-        y_vals = cos.(ω .* x_vals)
-        c = newton_int(x_vals, y_vals) # Are we allowed to use fit?
-
-        x_test_vals = range(a, stop=b, length=1000) # Higher precision?
-        abs_error = abs.(cos.(ω .* x_test_vals) - horner(c, x_vals, x_test_vals))
-
-        if maximum(abs_error) < tol
-            println("Subdivide Nodes: ", n)
-            return n
+        if curr < tol
+            # println("Subdivide Nodes: ", n + 1)
+            return n + 1
         end
-
         n += 1
     end
 end
-
-a = 0
-b = 3 * pi
-ω = 1
-tol = 1e-6
-
-# println("Subdivide: ", subdivide(a, b, ω, tol))
 
 """
 Computes Chebyshev nodes in the interval [a, b] for the function
@@ -162,47 +126,26 @@ Output
 """
 function chebyshev_nodes(a, b, ω, tol)
 
-    for i in 2:1000 # Can we just use subdivide? Too much computation power?
-        x_vals = zeros(i)
+    n = 2
 
-        for j in 1:i
-            x_vals[j] = 0.5 * (a + b) + 0.5 * (b - a) * cos(π * (2 * j + 1) / (2 * i + 2))
+    while n < 20
+        curr = ω^(n + 1) / 2^n / factorial(n + 1) * ((b - a) / 2)^(n + 1)
+
+        if curr < tol
+            break
         end
-
-        y_vals = cos.(ω .* x_vals)
-        c = newton_int(x_vals, y_vals) # Are we allowed to use fit?
-
-        x_test_vals = range(a, stop=b, length=1000) # Higher precision?
-        abs_error = abs.(cos.(ω .* x_test_vals) - horner(c, x_vals, x_test_vals))
-
-        if maximum(abs_error) < tol
-            println("Chebychev Nodes: ", i)
-            return x_vals
-        end
+        n += 1
     end
 
-    # while n < 1000000
+    # At this point n is the number of intervals
+    # n + 1 is the number of nodes
 
-    #     h = (b - a) / n
+    x = zeros(n + 1)
 
-    #     curr = ω^(n + 1) / 2^n / factorial(n + 1)
+    for i in 1:n+1
+        x[i] = 0.5 * (a + b) + 0.5 * (b - a) * cos(π * (2 * i - 1) / (2 * n + 2))
+    end
 
-    #     if curr < tol
-    #         break
-    #     end
-    #     n += 1
-    # end
-
-    # n = 17
-
-    # x = zeros(n)
-
-    # for i in 1:n
-    #     x[i] = 0.5 * (a + b) + 0.5 * (b - a) * cos(π * (2 * i + 1) / (2 * n + 2))
-    # end
-
-    # println("Chebyshev Nodes: ", n)
-    # return x
+    # println("Chebyshev Nodes: ", n + 1)
+    return x
 end
-
-# println("Chebyshev: ", chebyshev_nodes(a, b, ω, tol))
